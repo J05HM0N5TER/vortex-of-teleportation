90b8aad7d97c6f2196d06dc3f91525c7cb871a0a
Josh Francis <joshua.francis@live.com.au>
2020-12-06 09:49:14 +1100

    Seperated player into their own scene

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -11,11 +11,11 @@ public class Player : RigidBody2D
     private RigidBody2D rb;
 
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        // rb = GetNode<RigidBody2D>("Player");
-        GD.Print("Test");
-    }
+    // public override void _Ready()
+    // {
+    //     // rb = GetNode<RigidBody2D>("Player");
+    //     GD.Print("Test");
+    // }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
@@ -24,25 +24,7 @@ public class Player : RigidBody2D
         input.x -= Input.GetActionStrength("MoveLeft");
         input.x += Input.GetActionStrength("MoveRight");
         input *= delta * MoveSpeed;
-        // if (InputEvent.IsActionPressed("Jump"))
-        // {
+
         AddForce(Vector2.Zero, input);
-        // this.AddForce(Vector2.Zero, new Vector2(input));
-        // RigidBody2D rb = ((RigidBody2D) this).Addforce();
-        // instance.AddForce();
-        // G
-        // }
     }
-
-    // public override void _PhysicsProcess(float delta)
-    // {
-    //     Vector2 input = new Vector2();
-    //     input.x -= Input.GetActionStrength("MoveLeft");
-    //     input.x += Input.GetActionStrength("MoveRight");
-
-    //     MoveAndCollide(input.Normalized() * MoveSpeed * delta);
-    //     RigidBody2D rb = new RigidBody2D();
-    //     AddForce()
-    //     // rb.AddForce()
-    // }
 }
